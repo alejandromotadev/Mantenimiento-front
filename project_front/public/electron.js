cb8fc8703de96a4b4d341f87b4f3be2028a5d431
Gabriel <gabrielramondev23@gmail.com>
2023-01-21 11:47:28 -0600

    Inicio de proyecto electron con react

--- project_front/public/electron.js
+++ project_front/public/electron.js
@@ -1,24 +1,38 @@
-const electron = require("electron");
-const path = require("path");
-
+const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: { nodeIntegration: true, contextIsolation: false },
-  });
-  // and load the index.html of the app.
-  console.log(__dirname);
-  mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
+    mainWindow = new BrowserWindow({ width: 900, height: 680 });
+    mainWindow.loadURL(
+        isDev 
+        ? 'http://localhost:3000' 
+        : `file://${path.join(__dirname, '../build/index.html')}`);
+    if (isDev) {
+        // Open the DevTools.
+        //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+        // mainWindow.webContents.openDevTools();
+    }
+    mainWindow.on('closed', () => mainWindow = null);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow);
+
+if (require('electron-squirrel-startup')) app.quit();
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
--- project_front/src/App.js
+++ project_front/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Realm from "realm";
 
 function App() {
   return (
