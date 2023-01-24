965fa70c3ad3ce0cfdae4bb2a84625341ad70fae
alejandro mota <aamotadev@gmail.com>
2023-01-24 10:23:31 -0600

    formulario-js creado

---
+++ project_front/src/componentes/Formulario/Formulario.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Formulario() {
+  return (
+    <div>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Formulario;
