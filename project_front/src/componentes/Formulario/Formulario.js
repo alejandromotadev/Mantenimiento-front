2ed8d4d9970f00e451afd68f5db41cc59aa86587
alejandro mota <aamotadev@gmail.com>
2023-01-25 12:01:55 -0600

    'inputs divs'

--- project_front/src/componentes/Formulario/Formulario.js
+++ project_front/src/componentes/Formulario/Formulario.js
@@ -1,27 +1,52 @@
 import React from "react";
+import Style from "./formStyle.module.css";
+import Nav from "./navbar";
 
-function Formulario() {
-  return (
-    <div>
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+class Formulario extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className={Style.formWrapper}>
+          <form>
+            <div>
+              <h1>Proceso de diseno</h1>
+            </div>
+            <div>
+              <p>
+                Identificador: <b> a.a.a.a</b>
+              </p>
+            </div>
+            <div>
+              <h3>Nombre: </h3>
+              <input placeholder="Nombre"></input>
+            </div>
+            <div>
+              <h3>Fase: </h3>
+              <input placeholder="Fase"></input>
+            </div>
+            <div>
+              <h3>Responsable: </h3>
+              <input placeholder="Responsable"></input>
+            </div>
+            <div>
+              <h3>Participantes: </h3>
+              <input placeholder="Participantes"></input>
+            </div>
+            <div>
+              <h3>Proceso relacionado: </h3>
+              <input placeholder="Proceso relacionado"></input>
+            </div>
+            <div>
+              <h3>Evidencias de entrada</h3>
+            </div>
+            <div>
+              <h3>Evidencias de salida</h3>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default Formulario;
