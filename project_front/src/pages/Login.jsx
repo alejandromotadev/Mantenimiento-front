e2e5357dba356dcfb8ae5ff57860522d053b495c
Gabriel <gabrielramondev23@gmail.com>
2023-01-30 12:12:44 -0600

    Login funcionando sin retroalimentacion

--- project_front/src/pages/Login.jsx
+++ project_front/src/pages/Login.jsx
@@ -1,7 +1,35 @@
 import Style from "./Login.module.css";
-import { Link } from "react-router-dom";
+import { axiosInstance } from "../services/axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+	const [username, setUsername] = useState(null);
+	const [password, setPassword] = useState(null);
+	const navigate = useNavigate()
+
+	const login = async () => {
+		if (username !== null) {
+			if(password !==null) {
+				let data = {
+					"username" : username,
+					"password" : password
+				}
+				await axiosInstance.post("user/login/",data)
+				.then(res =>{
+					console.log(res.data);
+					localStorage.setItem('token',res.data.access_token)
+					navigate('/proceso')
+				})
+				.catch(error => alert(error));
+			}else{
+				console.log("No password provided")
+			}
+		} else {
+			console.log("No username provided");
+		}
+	};
+
 	return (
 		<div className={Style.main}>
 			<div className={Style.block}>
@@ -10,30 +38,31 @@ const Login = () => {
 				<h1 className={Style.titlo2}>Usuario</h1>
 
 				<div className={Style.inputcont}>
-					<i class="fa fa-user icon"></i>
+					<i className="fa fa-user icon"></i>
 					<input
 						className={Style.input}
 						type="email"
 						placeholder="Usuario"
+						onChange={(e) => setUsername(e.target.value)}
+						onBlur={(e) => setUsername(e.target.value)}
 					/>
 				</div>
 
 				<h1 className={Style.titulo2}>Contraseña</h1>
 
 				<div className={Style.inputcont}>
-					<i class="fa fa-unlock-alt"></i>
+					<i className="fa fa-unlock-alt"></i>
 					<input
 						className={Style.input}
 						type="password"
 						placeholder="Password"
+						onChange={(e) => setPassword(e.target.value)}
+						onBlur={(e) => setPassword(e.target.value)}
 					/>
 				</div>
 
 				<div className={Style.wrapper}>
-					<Link to="/proceso">
-						{" "}
-						<button className={Style.button63}>Entrar</button>{" "}
-					</Link>
+					<button className={Style.button63} onClick={login}>Entrar</button>
 				</div>
 			</div>
 		</div>
