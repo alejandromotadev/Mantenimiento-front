f3c2d3a05d7910308f55127511c2e009bbcb3f24
JorgeCamToledo <camacho.ja0402@gmail.com>
2023-02-28 09:56:20 -0600

    cambios menores

--- project_front/src/App.jsx
+++ project_front/src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Fases from "./pages/Fases";
-import Formulario from "./pages/Formulario";
 import FormularioProceso from "./pages/FormularioProceso";
 import Login from "./pages/Login";
 import Manual from "./pages/FormularioManual";
-import Proyectos from "./pages/Proyectos";
+import Proyectos from "./pages/Proyectos";  
 import Procesos from "./pages/Procesos";
 
 function App() {
@@ -15,12 +14,10 @@ function App() {
         <Routes>
         <Route path="/" element={<Login />} />
           <Route path="/proceso_form/" element={<FormularioProceso />} />
-          {/* <Route path="/proceso/:faseId" element={<Formulario />} /> */}
           <Route path="/manual" element={<Manual />} />
           <Route path="/proceso/create" element={<Procesos />} />
           <Route path="/inicio" element={<Proyectos />} />
           <Route path="/fases" element={<Fases />} />
-          <Route path="/proyectos" element={<Formulario />}/>
         </Routes>
       </Router>
     </div>
